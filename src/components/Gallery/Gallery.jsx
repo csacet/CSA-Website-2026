181ce78d0daec95af41b49439386b96bbb80f911
Rohit Mendus <rohit.mendus@ipsrsolutions.com>
2026-08-27 02:39:29 +0530

    Made lines and pizel art unifrom across all section headings

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -48,20 +48,14 @@ export default function Gallery() {
 
   return (
     <section className="gallery-section">
-      <div className="gallery-title-container">
-        <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg" className="dinosaur-logo-gallery">
-          <path d="M30.8 0V2.8H28V19.6H25.2V22.4H19.6V25.2H16.8V28H14V30.8H8.4V28H5.6V25.2H2.8V19.6H0V36.4H2.8V39.2H5.6V42H8.4V44.8H11.2V56H16.8V53.2H14V50.4H16.8V47.6H19.6V44.8H22.4V47.6H25.2V56H30.8V53.2H28V42H30.8V39.2H33.6V36.4H36.4V28H39.2V30.8H42V25.2H36.4V19.6H50.4V16.8H42V14H56V2.8H53.2V0M33.6 2.8H36.4V5.6H33.6V2.8Z" fill="#f8f6f4" />
+      <div className="csa-section-heading gallery-heading">
+        <svg className="csa-heading-dino" width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path d="M30.8 0V2.8H28V19.6H25.2V22.4H19.6V25.2H16.8V28H14V30.8H8.4V28H5.6V25.2H2.8V19.6H0V36.4H2.8V39.2H5.6V42H8.4V44.8H11.2V56H16.8V53.2H14V50.4H16.8V47.6H19.6V44.8H22.4V47.6H25.2V56H30.8V53.2H28V42H30.8V39.2H33.6V36.4H36.4V28H39.2V30.8H42V25.2H36.4V19.6H50.4V16.8H42V14H56V2.8H53.2V0M33.6 2.8H36.4V5.6H33.6V2.8Z" fill="currentColor" />
         </svg>
-        <div className="gallery-title-section">
-          <div className="gallery-title-frame">
-            <div className='gallery-line gallery-line-left'></div>
-            <h1 className="gallery-title">Gallery</h1>
-            <div className='gallery-line gallery-line-right'></div>
-          </div>
-          <div className="gallery-border-frame">
-            <div className='gallery-border-top'></div>
-            <div className='gallery-border-bottom'></div>
-          </div>
+        <div className="csa-section-heading-frame">
+          <span className="csa-section-heading-line" />
+          <h1 className="csa-section-heading-title">Gallery</h1>
+          <span className="csa-section-heading-line" />
         </div>
       </div>
 
